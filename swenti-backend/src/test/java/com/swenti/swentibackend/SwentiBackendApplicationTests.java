132e8ce7d348845ed541cf651c1e43c4ece7e07c
TNALSA <msa1859@naver.com>
2024-06-24 13:20:29 +0900

    전체 프로젝트

---
+++ swenti-backend/src/test/java/com/swenti/swentibackend/SwentiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swenti.swentibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwentiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
