238dd6c01ba4f4737b15bcd7264abc086630890d
TNALSA <msa1859@naver.com>
2024-06-25 09:56:34 +0900

    crawling: 네이버 기사 크롤링

--- swenti-crawling/crawling_naver.py
+++ swenti-crawling/crawling_naver.py
@@ -7,4 +7,27 @@ headers = {
     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
 }
 res = requests.get(url, headers=headers)
+res.raise_for_status()
+
 soup = BeautifulSoup(res.text, "lxml")
+
+news_list = soup.find("ul", attrs={"class" : "sa_list"}).find_all("li")
+for news in news_list:
+    title = news.find("div",attrs={"class" : "sa_text"}).find("a").get_text().strip()
+    link = news.find("div",attrs={"class" : "sa_text"}).find("a")["href"]
+    publisher = news.find("div",attrs={"class" : "sa_text"}).find("div",attrs={"class" : "sa_text_press"}).get_text().strip()
+    image_url = news.find("div", attrs={"class" : "sa_thumb_inner"}).find("img",attrs={"class" : "_LAZY_LOADING _LAZY_LOADING_INIT_HIDE"})["data-src"]
+    if image_url:
+        image_url = news.find("div", attrs={"class" : "sa_thumb_inner"}).find("img",attrs={"class" : "_LAZY_LOADING _LAZY_LOADING_INIT_HIDE"})["data-src"]
+    else:
+        image_url = "이미지가 없습니다."
+
+    print("기사 제목: "+title)
+    print("링크: "+link)
+    print("언론사: "+publisher)
+    print("이미지: "+image_url)
+    # image_res = requests.get(image_url)
+    # image_res.raise_for_status()
+    #
+    # with open("{}.jpg".format(title), "wb") as f:
+    #     f.write(image_res.content)
