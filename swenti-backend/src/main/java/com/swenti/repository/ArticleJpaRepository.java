76818580ff4a240255ab0a7f316b5afc816ba32f
TNALSA <msa1859@naver.com>
2024-07-05 13:16:50 +0900

    feat: 사이트 별 작및 작성 일자 순 기사리스트 조회

--- swenti-backend/src/main/java/com/swenti/repository/ArticleJpaRepository.java
+++ swenti-backend/src/main/java/com/swenti/repository/ArticleJpaRepository.java
@@ -11,7 +11,7 @@ public interface ArticleJpaRepository extends JpaRepository<Article, Integer> {
     @Query("select a from Article a where date_format(a.writed_date,'%Y-%m-%d') = date_format(now(),'%Y-%m-%d')")
     List<Article> LookupArticle();
 
-    @Query("select a from Article a where date_format(a.writed_date,'%Y-%m-%d') = date_format(now(),'%Y-%m-%d') and a.site = :site")
+    @Query("select a from Article a where date_format(a.writed_date,'%Y-%m-%d') = date_format(now(),'%Y-%m-%d') and a.site = :site order by a.writed_date desc")
     List<Article> LookupBySite(String site);
 
     @Query("SELECT a FROM Article a WHERE a.id = :id")
